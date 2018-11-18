8cb3d58431eb633fa359226bc49e8c9adc5eb4aa
Pablo Schlusen <pablo.schlusen@gmail.com>
2018-11-18 15:13:57 -0200

    adicionar arquivo

---
+++ src/eFitness/negocio/TreinoNegocio.java
@@ -0,0 +1,68 @@
+package efitness.negocio;
+
+import efitness.dao.db.TreinoDAOBD;
+import java.util.List;
+import efitness.model.Treino;
+import efitness.dao.TreinoDAO;
+
+/**
+ *
+ * @author Pablo Schlusen
+ */
+public class TreinoNegocio {
+    private final TreinoDAO treinoDAO;
+    
+    public TreinoNegocio(){
+        this.treinoDAO = new TreinoDAOBD();
+    }
+    
+    public void salvar(Treino treino) throws NegocioException {
+        this.validarCamposObrigatorios(treino);
+        treinoDAO.salvar(treino);
+    }
+    
+    public List<Treino> listar(){
+        return (treinoDAO.listar());
+    }
+    
+    public void deletar(Treino treino) throws NegocioException {
+        if (treino == null || treino.getId() == 0) {
+            throw new NegocioException("Treino nao existe");
+        }
+        treinoDAO.deletar(treino);
+    }
+    
+    public void atualizar(Treino treino) throws NegocioException {
+        if (treino == null || treino.getId() == 0) {
+            throw new NegocioException("Treino nao existe");
+        }
+        treinoDAO.atualizar(treino);
+    }
+    public Treino procurarPorID(int id) throws NegocioException {
+        Treino treino = treinoDAO.procurarPorId(id);
+        
+        if (treino == null) {
+            throw new NegocioException("Treino nao encontrado");
+        }
+        return (treino);
+    }
+   
+    
+    
+    
+    private void validarCamposObrigatorios(Treino treino) throws NegocioException {
+        if (treino.getObjetivo() == null || treino.getObjetivo().isEmpty()) {
+            throw new NegocioException("Objetivo nao informado");
+        }
+        
+        if (treino.getAluno() == null) {
+            throw new NegocioException("Aluno nao informado!");
+        }
+    }
+    
+   
+
+    public String procurarPorID(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
